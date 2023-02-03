eaa2053242b09005177e982383d7d44751bbccb4
Agustin Alexander <agustin.c.alexander@gmail.com>
2023-02-03 19:14:35 -0300

    added api

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify, request
+from ibflex import client, parser
+
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+
+app = create_app()
+
+
+@app.route('/api/v1/flex-query', methods=['GET'])
+def get_accountStatement():
+    ibkrtoken = request.headers['IBKR-TOKEN']
+    ibkrquery = request.headers['IBKR-QUERY']
+    response = client.download(ibkrtoken, ibkrquery)
+    return jsonify(parser.parse(response).__dict__)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
