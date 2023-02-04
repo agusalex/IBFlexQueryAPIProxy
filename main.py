772d8c8d59d5fecbfc56e1045bbedb7c72e008b6
Agustin Alexander <agustin.c.alexander@gmail.com>
2023-02-03 21:17:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def get_accountStatement():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(port=int(os.environ.get("PORT", 8080)))
