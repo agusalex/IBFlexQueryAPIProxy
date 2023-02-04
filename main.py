a44776df365bf7b1bc896786e69cf04a2b9f02dd
Agustin Alexander <agustin.alexander@salesforce.com>
2023-02-04 09:19:22 -0300

    added endpoint

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 from flask import Flask
 from flask import jsonify, request
-from ibflex import client, parser
+from ibflex import client, parser, FlexQueryResponse, Trade
+from datetime import datetime
 
 
 def create_app():
@@ -19,6 +20,37 @@ def get_accountStatement():
     ibkrquery = request.headers['IBKR-QUERY']
     response = client.download(ibkrtoken, ibkrquery)
     return jsonify(parser.parse(response).__dict__)
+
+
+@app.route('/api/v1/ghostfolio-trades', methods=['GET'])
+# For ghostfolio
+def get_trades():
+    ibkrtoken = request.headers['IBKR-TOKEN']
+    ibkrquery = request.headers['IBKR-QUERY']
+    response = client.download(ibkrtoken, ibkrquery)
+    query: FlexQueryResponse = parser.parse(response)
+    activities = []
+    date_format = "%Y-%m-%d"
+    for trade in query.FlexStatements[0].Trades:
+        if trade.openCloseIndicator.CLOSE:
+            date = datetime.strptime(str(trade.tradeDate), date_format)
+            iso_format = date.isoformat()
+            symbol = trade.symbol
+            if ".USD-PAXOS" in trade.symbol:
+                symbol = trade.symbol.replace(".USD-PAXOS", "") + "USD"
+
+            activities.append({
+                "currency": trade.currency,
+                "dataSource": "YAHOO",
+                "date": iso_format,
+                "fee": float(0),
+                "quantity": float(trade.quantity),
+                "symbol": symbol,
+                "type": trade.buySell,
+                "unitPrice": float(trade.tradePrice)
+            })
+    activities = sorted(activities, key=lambda x: x["date"])
+    return jsonify({"activities": activities})
 
 
 if __name__ == '__main__':
